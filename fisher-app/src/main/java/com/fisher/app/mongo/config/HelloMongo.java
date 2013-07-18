9f86127723a40780fa9cbb683aae6b974e4b16b8
makotolusi <makoto.deadpool@gmail.com>
2013-07-18 18:32:00 +0800

    spring mongo maven multi model init

---
+++ fisher-app/src/main/java/com/fisher/app/mongo/config/HelloMongo.java
@@ -0,0 +1,36 @@
+package com.fisher.app.mongo.config;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.stereotype.Repository;
+
+import com.fisher.app.domain.Account;
+import com.fisher.app.domain.Person;
+
+@Repository
+public class HelloMongo {
+
+	@Autowired
+	MongoOperations mongoOperations;
+
+	public void run() {
+
+		if (mongoOperations.collectionExists(Person.class)) {
+			mongoOperations.dropCollection(Person.class);
+		}
+
+		mongoOperations.createCollection(Person.class);
+
+		Person p = new Person("John", 39);
+		Account a = new Account("1234-59873-893-1", Account.Type.SAVINGS, 123.45D);
+		p.getAccounts().add(a);
+
+		mongoOperations.insert(p);
+
+		List<Person> results = mongoOperations.findAll(Person.class);
+		System.out.println("Results: " + results);
+	}
+
+}
